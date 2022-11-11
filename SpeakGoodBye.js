ab2229048cc2010656aa7579e74abf92fa2d1b86
Dean758 <115797127+Dean758@users.noreply.github.com>
2022-11-11 10:25:50 -0400

    Tweaks

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -3,6 +3,7 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
+
 (function () {
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
@@ -31,7 +32,7 @@ var speakWord = "Good Bye";
        }
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
+
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
